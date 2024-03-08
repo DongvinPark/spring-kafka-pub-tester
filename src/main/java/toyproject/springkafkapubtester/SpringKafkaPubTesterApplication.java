34236ae22919ed418dca1e9907c542dbde1f4615
DongvinPark <dongvin99@naver.com>
2024-03-08 16:22:36 +0900

    initial commit

---
+++ src/main/java/toyproject/springkafkapubtester/SpringKafkaPubTesterApplication.java
@@ -0,0 +1,13 @@
+package toyproject.springkafkapubtester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaPubTesterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaPubTesterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/toyproject/springkafkapubtester/SpringKafkaPubTesterApplicationTests.java
@@ -0,0 +1,13 @@
+package toyproject.springkafkapubtester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaPubTesterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
